c7ec5450bb4b8c40a951032ff7fa00e2bcc9a691
Mahan <mahanrn@student.ubc.ca>
2025-06-30 21:21:03 -0700

    Added topics view

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -5,5 +5,7 @@ from . import views
 app_name = 'learning_logs'
 urlpatterns = [
     # Home Page
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    # Page that shows all topics
+    path('topics/', view.topics, name='topics'),
 ]
