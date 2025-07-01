820c7ace551272ea744c53a7d4422e899f3c0120
Mahan <mahanrn@student.ubc.ca>
2025-06-30 22:30:50 -0700

    Added URL and view for each individual topic

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -7,5 +7,7 @@ urlpatterns = [
     # Home Page
     path('', views.index, name='index'),
     # Page that shows all topics
-    path('topics/', view.topics, name='topics'),
+    path('topics/', views.topics, name='topics'),
+    # Detail page for a single topic
+    path('topics/<int:topic_id>/', views.topic, name='topic'),
 ]
