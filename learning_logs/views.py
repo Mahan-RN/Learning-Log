0dd0b73fc448111f8492119466139ae02a4ce1d3
Mahan <mahanrn@student.ubc.ca>
2025-07-02 16:56:26 -0700

    Added view for edit_entry

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from .models import Topic
+from .models import Topic, Entry
 from .forms import TopicForm, EntryForm
 
 # Create your views here.
@@ -45,7 +45,7 @@ def new_entry(request, topic_id):
     """Add a new entry for a particular topic"""
     topic = Topic.objects.get(id=topic_id)
 
-    if request.method != 'POST':
+    if request.method != "POST":
         # No data submitted; create a blank form
         form = EntryForm()
     else:
@@ -59,3 +59,22 @@ def new_entry(request, topic_id):
     # Display a blank or invalid from
     context = {"topic": topic, "form": form}
     return render(request, "learning_logs/new_entry.html", context)
+
+
+def edit_entry(request, entry_id):
+    """Edit an existing entry"""
+    entry = Entry.objects.get(id=entry_id)
+    topic = entry.topic
+
+    if request.method != "POST":
+        # Initial request; pre-fill form with the current entry
+        form = EntryForm(instance=entry)
+    else:
+        # POST data submitted; process data
+        form = EntryForm(instance=entry, data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("learning_logs:topic", topic_id=topic.id)  # type: ignore
+
+    context = {"entry": entry, "topic": topic, "form": form}
+    return render(request, "learning_logs/edit_entry.html", context)
