36adacf85a0ea03b7052466f9125f408f7df385d
Mahan <mahanrn@student.ubc.ca>
2025-07-02 16:39:48 -0700

    Added missing argument to new_entry

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -45,12 +45,12 @@ def new_entry(request, topic_id):
     """Add a new entry for a particular topic"""
     topic = Topic.objects.get(id=topic_id)
 
-    if request.method != "POST":
+    if request.method != 'POST':
         # No data submitted; create a blank form
         form = EntryForm()
     else:
         # POST data submitted; process data
-        form = EntryForm()
+        form = EntryForm(data=request.POST)
         if form.is_valid():
             new_entry = form.save(commit=False)
             new_entry.topic = topic
